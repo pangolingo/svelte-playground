2b2550a339d03b77932f4401884809d85ad723f8
Dave Iverson <daveiverson@thoughtbot.com>
2023-09-28 20:22:28 -0500

    Styled the pokemon detail page

--- src/routes/pokemon/[id]/+page.svelte
+++ src/routes/pokemon/[id]/+page.svelte
@@ -1,36 +1,42 @@
 <script lang="ts">
+  import { enhance } from '$app/forms';
+  import PokemonPreview from '$lib/components/pokemon/PokemonPreview.svelte';
   import type { ActionData, PageData } from './$types';
 
   export let data: PageData;
   export let form: ActionData;
 </script>
 
-<h2 class="h2 uppercase">{data.pokemon.name}</h2>
+<div class="grid grid-cols-2 max-w-2xl gap-4">
+  <PokemonPreview pokemon={data.pokemon} favorite={data.isSaved} />
+  <div class="flex flex-col">
+    <section class="flex-1">
+      <h3 class="h3 mt-2">Abilities</h3>
+      <ul>
+        {#each data.pokemon.abilities as ability}
+          <li>
+            {ability.ability.name}
+          </li>
+        {/each}
+      </ul>
+    </section>
 
-<form method="post" action="?/save">
-  <input type="hidden" name="pokemon_id" value={data.pokemon.id} />
-  <input type="hidden" name="pokemon_name" value={data.pokemon.name} />
-  {#if data.isSaved}
-    <button class="btn" type="submit" formaction="?/unsave">Remove saved pokemon</button>
-  {:else}
-    <button class="btn" type="submit">Save pokemon</button>
-  {/if}
-  {#if form?.success}
-    {#if data.isSaved}<p>Saved!</p>{/if}
-    {#if !data.isSaved}<p>Unsaved!</p>{/if}
-  {/if}
-  {#if form?.unauthorized}
-    <p>Please log in to save this pokemon.</p>
-  {/if}
-</form>
+    <form method="post" action="?/save" use:enhance>
+      <input type="hidden" name="pokemon_id" value={data.pokemon.id} />
+      <input type="hidden" name="pokemon_name" value={data.pokemon.name} />
+      {#if form?.success}
+        {#if data.isSaved}<p>Saved!</p>{/if}
+        {#if !data.isSaved}<p>Unsaved!</p>{/if}
+      {/if}
+      {#if form?.unauthorized}
+        <p>Please log in to save this pokemon.</p>
+      {/if}
+      {#if data.isSaved}
+        <button class="btn" type="submit" formaction="?/unsave">💔 Remove saved pokemon</button>
+      {:else}
+        <button class="btn" type="submit">❤️ Save pokemon</button>
+      {/if}
+    </form>
 
-<img src={data.pokemon.sprites.front_default} alt="the pokemon sprite from the game" />
-
-<h3 class="h3">Abilities</h3>
-<ul>
-  {#each data.pokemon.abilities as ability}
-    <li>
-      {ability.ability.name}
-    </li>
-  {/each}
-</ul>
+  </div>
+</div>
