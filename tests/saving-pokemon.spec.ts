f4769b679258d30d8dd0d3cb1a3e32f223bd1a90
Dave Iverson <dave@ivrsn.com>
2023-09-30 16:26:59 -0500

    Wrote a test for the favorite pokemon api route

--- tests/saving-pokemon.spec.ts
+++ tests/saving-pokemon.spec.ts
@@ -1,14 +1,24 @@
 import 'dotenv/config';
 import { test, expect } from '@playwright/test';
 
-test('trying to save pokemon when logged out', async ({ page }) => {
-  await page.goto('/');
-  await page.getByRole('link', { name: 'Check out all the pokemons!' }).click();
-  await page.getByRole('link', { name: '#4 charmander' }).click();
-  await page.getByRole('button', { name: 'Save pokemon' }).click();
-  await expect(page.getByText('Please log in to save this pokemon.')).toBeVisible();
-  await expect(page.getByRole('button', { name: 'Save pokemon' })).toBeVisible();
-  await expect(page.getByRole('button', { name: 'Remove saved pokemon' })).not.toBeVisible();
+test.describe('when logged out', () => {
+  test('trying to save pokemon shows an error', async ({ page }) => {
+    await page.goto('/');
+    await page.getByRole('link', { name: 'Check out all the pokemons!' }).click();
+    await page.getByRole('link', { name: '#4 charmander' }).click();
+    await page.getByRole('button', { name: 'Save pokemon' }).click();
+    await expect(page.getByText('Please log in to save this pokemon.')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Save pokemon' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Remove saved pokemon' })).not.toBeVisible();
+  });
+
+  test('the API route returns unauthorized', async ({ request }) => {
+    const apiPokemon = await request.get('/pokemon/favorites');
+    expect(apiPokemon.status()).toBe(401);
+    expect(await apiPokemon.json()).toEqual({
+      unauthorized: true
+    });
+  });
 });
 
 test.describe('when logged in as a user', () => {
@@ -42,5 +52,42 @@ test.describe('when logged in as a user', () => {
     await page.goto('/pokemon');
     await expect(page.getByRole('link', { name: '❤️ #4 charmander' })).not.toBeVisible();
     await expect(page.getByRole('link', { name: '#4 charmander' })).toBeVisible();
+  });
+
+  test(' the API route shows saved pokemon', async ({ page, request }) => {
+    await page.goto('/pokemon');
+    await page.getByRole('link', { name: '#4 charmander' }).click();
+    await page.getByRole('button', { name: 'Save pokemon' }).click();
+    await expect(page.getByText('Saved!')).toBeVisible();
+
+    // saved pokemon appears in the API
+    let apiPokemon = await request.get('/pokemon/favorites');
+    expect(apiPokemon.ok()).toBeTruthy();
+    expect(await apiPokemon.json()).toEqual(
+      expect.objectContaining({
+        favoritePokemon: expect.arrayContaining([
+          {
+            id: expect.anything(),
+            pokemon_name: 'charmander',
+            pokemon_id: 4
+          }
+        ])
+      })
+    );
+
+    // unsaving
+    await page.goto('/pokemon');
+    await page.getByRole('link', { name: '❤️ #4 charmander' }).click();
+    await page.getByRole('button', { name: 'Remove saved pokemon' }).click();
+    await expect(page.getByText('Unsaved!')).toBeVisible();
+
+    // pokemon no longers appears in the API
+    apiPokemon = await request.get('/pokemon/favorites');
+    expect(apiPokemon.ok()).toBeTruthy();
+    expect(await apiPokemon.json()).toEqual(
+      expect.objectContaining({
+        favoritePokemon: []
+      })
+    );
   });
 });
